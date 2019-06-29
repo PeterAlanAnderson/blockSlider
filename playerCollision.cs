9227e5e3ec15c5a2d9cb67c6a4412669b9959200
unknown <peter.alan.anderson@gmail.com>
2019-06-28 23:26:36 -0500

    initial commit

---
+++ playerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+
+{
+    public playerMovement movement;
+
+    void OnCollisionEnter( Collision collisionInfo) {
+        if (collisionInfo.collider.tag == "Obstacle"){
+            Debug.Log("obstacle struck");
+            movement.enabled = false;
+        }
+    }
+}
---
+++ playerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        // times Time.deltaTime means essentially to not couple to the frame rate */
+
+        if( Input.GetKey ("d") ) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if( Input.GetKey ("a") ) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        
+    }
+}
